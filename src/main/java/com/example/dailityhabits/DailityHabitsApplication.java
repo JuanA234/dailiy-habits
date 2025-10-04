ab906173c9ed232d3a064cf5850836322541fe42
Juan Avendaño <juanandreslujan30@gmail.com>
2025-10-04 15:09:28 -0500

    init

---
+++ src/main/java/com/example/dailityhabits/DailityHabitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dailityhabits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailityHabitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailityHabitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dailityhabits/DailityHabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailityhabits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailityHabitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
