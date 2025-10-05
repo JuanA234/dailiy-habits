f6f5dbfcd7dfabbdfc83203a6193ce3e92d74cfc
Juan Avendaño <juanandreslujan30@gmail.com>
2025-10-05 09:45:26 -0500

    tilin

--- src/main/java/com/example/dailityhabits/service/interfaces/RegisterCompletedService.java
+++ src/main/java/com/example/dailityhabits/service/interfaces/RegisterCompletedService.java
@@ -2,10 +2,16 @@ package com.example.dailityhabits.service.interfaces;
 
 import com.example.dailityhabits.DTO.habit.UpdateHabitDTO;
 import com.example.dailityhabits.DTO.registerCompleted.ResponseRegisterCompletedDTO;
+import com.example.dailityhabits.DTO.registerCompleted.UpdateRegisterCompletedDTO;
 
 public interface RegisterCompletedService {
 
-    ResponseRegisterCompletedDTO UpdateRegister(Long id, UpdateHabitDTO request);
+    ResponseRegisterCompletedDTO listRegisterCompleted();
+
+    ResponseRegisterCompletedDTO findRegisterCompleted(Long id);
+
+
+    ResponseRegisterCompletedDTO UpdateRegister(Long id, UpdateRegisterCompletedDTO request);
 
     void deleteRegister(Long id);
 }
