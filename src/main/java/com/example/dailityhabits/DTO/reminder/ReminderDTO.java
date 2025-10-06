753d6b8fd984a7303f953747cbcb142017478e02
leonelp <pereiraleonel287@gmail.com>
2025-10-05 21:33:20 -0500

    adding validations to dto of Reminder and Frequency

--- src/main/java/com/example/dailityhabits/DTO/reminder/ReminderDTO.java
+++ src/main/java/com/example/dailityhabits/DTO/reminder/ReminderDTO.java
@@ -1,13 +1,24 @@
 package com.example.dailityhabits.DTO.reminder;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.List;
 
 public record ReminderDTO(
+        @Positive(message = "El ID debe ser un número positivo")
         Long id,
+
+        @NotNull(message = "La hora es obligatoria")
         LocalTime time,
+
         boolean active,
-        List<DayOfWeek>days
+
+        @NotNull(message = "Los días no pueden ser nulos")
+        @Size(max = 7, message = "No puede tener más de 7 días")
+        List<DayOfWeek> days
 ) {
 }
